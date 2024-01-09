adff99908d4f32f44ab357b47f26dd381376ee84
Doug Hubbard <doug@dougis.com>
2024-01-08 21:28:12 -0800

    Added error middleware, started on redux for state management

---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
+
+const store = configureStore({
+  reducer: { [apiSlice.reducerPath]: apiSlice.reducer, cart: cartSliceReducer },
+  middleware: (buildGetDefaultMiddleware) =>
+    buildGetDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export default store;
