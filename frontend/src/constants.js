7328e788148d1514114ce3f7f3659dfad0fc01f3
Doug Hubbard <doug@dougis.com>
2024-02-19 10:54:38 -0800

    renamed the constants to differentiate between FE and BE urls

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,8 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 export const BASE_URL = "";
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
+export const PRODUCTS_API_URL = "/api/products";
+export const USERS_API_URL = "/api/users";
+export const ORDERS_API_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
+export const ORDERS_URL = "/orders";
